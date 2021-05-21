05925c0c0154873dd2f98cee2566880bb9fbd676
crisalejos <cristina-alejos@outlook.com>
2021-05-21 11:07:05 +0200

    Corrections avec variables dolar + clés

--- js-6.js
+++ js-6.js
@@ -1,5 +1,6 @@
 function submitInfo() {
     let firstNumber = document.getElementById('firstNumber').value;
     let secondNumber = document.getElementById('secondNumber').value;                
-    document.write('<p>Premier nombre = ' + firstNumber  + '</p><p>Deuxième nombre = '  + secondNumber +  '</p><p>' + firstNumber + '/' + secondNumber + '=' + firstNumber/secondNumber + '</p>') ;
+    //  division
+    document.write(`<p>Premier nombre = ${firstNumber}</p><p>Deuxième nombre = ${secondNumber} </p><p>Afficher le reste de la division de deux nombres. ${firstNumber} + ${secondNumber} = ${firstNumber/secondNumber} </p>`) ;
 }
